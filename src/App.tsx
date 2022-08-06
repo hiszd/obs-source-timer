ab69fc6448ac1989e3fbb22b5bcc79716bfceff6
Zion Koyl <hiszd1@gmail.com>
2022-08-06 08:13:50 -0400

    IP 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import styles from './App.module.css';
 import { createEffect, createSignal, onMount } from 'solid-js';
 import { createSocket, obs, obsApi } from '../lib/obs';
 import { Timer } from '../lib/Timer';
+import { getSuspenseContext } from 'solid-js/types/reactive/signal';
 
 function App() {
 
-  const [scene, setScene] = createSignal('Connecting...');
-  const [source, setSource] = createSignal('Connecting...');
+  const [scene, setScene] = createSignal({ sceneIndex: 0, sceneName: 'Connecting...' });
+  const [source, setSource] = createSignal({ sceneItemId: 0, sourceName: 'Connecting...' });
 
   const [scenes, setScenes] = createSignal([{ sceneIndex: 0, sceneName: 'Connecting...' }]);
   const [sources, setSources] = createSignal([{ sceneIndex: 0, sceneName: 'Connecting...' }]);
@@ -28,13 +29,18 @@ function App() {
     }
   });
 
-  let identTimer = setInterval(() => {
-    if (obs.identified) {
+  createEffect(() => {
+    if (identified()) {
       obsApi('scenelist').then((e) => {
         console.log(e);
         setScene(e.scenes[0]);
         setScenes(e.scenes);
       });
+    }
+  });
+
+  let identTimer = setInterval(() => {
+    if (obs.identified) {
       clearInterval(identTimer);
     }
   }, 1000);
@@ -51,6 +57,11 @@ function App() {
     }, 4000);
   });
 
+  const getSceneFromName = (sceneName: string) => {
+    let scene = scenes().find(e => e.sceneName == sceneName);
+    return scene;
+  }
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -58,7 +69,11 @@ function App() {
           Welcome to <span>OBS Source Timer</span>
         </h1>
         <div>
-          <select class={styles.selectList} size={6}>
+          <select class={styles.selectList} onChange={(e) => {
+            if (e.target.value != 'Connecting...') {
+              setScene(getSceneFromName(e.target.value));
+            }
+          }} size={6}>
             <For each={scenes()}>{(scene) =>
               <option value={scene.sceneName}>{scene.sceneIndex}: {scene.sceneName}</option>
             }</For>
